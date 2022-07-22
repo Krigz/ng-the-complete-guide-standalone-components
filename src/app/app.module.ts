45a3eb6c9bce36a36cdf74069c2dd7e62effaf89
Rik Vergauwen <rik.vergauwen@gmail.com>
2022-07-22 11:32:49 +0200

    348

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  // declarations: [AppComponent, WelcomeComponent, DetailsComponent], // if it's not standalone, declare the cmp
-  declarations: [AppComponent, WelcomeComponent], // if it's standalone, don't declare
-  imports: [BrowserModule, DetailsComponent], // add it in imports instead
+  declarations: [AppComponent],
+  imports: [BrowserModule, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
